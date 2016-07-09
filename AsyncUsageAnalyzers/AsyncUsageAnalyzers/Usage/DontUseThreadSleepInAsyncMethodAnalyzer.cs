77b7e2b415a58d7f6ee87d745f1526608027065c
Tomasz Maczyński <t.maczynski@gmail.com>
2016-07-09 22:51:08 +0200

    first (kinda) working implementation of DontUseThreadSleepInAsyncMethodAnalyzer, update of unit test
    implementations registers on method invocations expressions
    column number is updates in unit tests
    tests still fails because of wrong descriptor/message

--- AsyncUsageAnalyzers/AsyncUsageAnalyzers/Usage/DontUseThreadSleepInAsyncMethodAnalyzer.cs
+++ AsyncUsageAnalyzers/AsyncUsageAnalyzers/Usage/DontUseThreadSleepInAsyncMethodAnalyzer.cs
@@ -14,12 +14,13 @@ namespace AsyncUsageAnalyzers.Usage
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
     using System.Collections.Immutable;
     using System.Collections.Concurrent;
-    
 
     /// <summary>
-    /// This analyzer reports a diagnostic if an asynchronous if Thread.Sleep() method is called inside async method.
+    /// This analyzer reports a diagnostic if Thread.Sleep() method is called inside async method.
     /// </summary>
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     internal class DontUseThreadSleepInAsyncMethodAnalyzer : DiagnosticAnalyzer
@@ -53,7 +54,7 @@ namespace AsyncUsageAnalyzers.Usage
             new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning,
                 AnalyzerConstants.EnabledByDefault, Description, HelpLink);
 
-        private static readonly Action<CompilationStartAnalysisContext> CompilationStartAction = HandleCompilationStart;
+        private static readonly Action<SyntaxNodeAnalysisContext> HandleMethodDeclarationAction = HandleMethodDeclaration;
 
         /// <inheritdoc/>
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
@@ -62,150 +63,45 @@ namespace AsyncUsageAnalyzers.Usage
         /// <inheritdoc/>
         public override void Initialize(AnalysisContext context)
         {
-            context.RegisterCompilationStartAction(CompilationStartAction);
+            // This lines can be commented out when Microsoft.CodeAnalysis is upgraded from version 1.0.0.0 to a version that supports that operations
+            // context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            // context.EnableConcurrentExecution();
+            //
+            context.RegisterSyntaxNodeAction(HandleMethodDeclarationAction, SyntaxKind.MethodDeclaration);
         }
 
-        private static void HandleCompilationStart(CompilationStartAnalysisContext context)
+        private static void HandleMethodDeclaration(SyntaxNodeAnalysisContext context)
         {
-            Analyzer analyzer = new Analyzer(
-                context.Compilation.GetOrCreateGeneratedDocumentCache(),
-                context.Compilation);
-            context.RegisterSymbolAction(analyzer.HandleMethodDeclaration, SymbolKind.Method);
+            var methodDeclaration = (MethodDeclarationSyntax)context.Node;
+            if (!HasAsyncMethodModifier(methodDeclaration))
+            {
+                return;
+            }
+
+            var threadTypeMetadata = context.SemanticModel.Compilation.GetTypeByMetadataName("System.Threading.Thread");
+
+            var invocationsWithinTheMethod = context.Node.DescendantNodes()
+                .OfType<InvocationExpressionSyntax>();
+
+            var invocationsOfThreadSleep = invocationsWithinTheMethod
+                .Where(invocation =>
+                {
+                    var methodSymbol = context.SemanticModel.GetSymbolInfo(invocation).Symbol as IMethodSymbol;
+                    return methodSymbol != null
+                           && methodSymbol.Name == "Sleep"
+                           && methodSymbol.ContainingNamespace.Name == "Threading"; // TODO: add additional checks
+                });
+
+            foreach (var invocation in invocationsOfThreadSleep)
+            {
+                //var methodName = methodDeclaration.Identifier.Text;
+                context.ReportDiagnostic(Diagnostic.Create(Descriptor, invocation.GetLocation()));
+            }
         }
 
-        // TODO: remove code duplication between this and IncludeCancellationParameterAnalyzer's Analyzer inner class
-        private class Analyzer
+        private static bool HasAsyncMethodModifier(MethodDeclarationSyntax methodDeclaration)
         {
-            private readonly ConcurrentDictionary<SyntaxTree, bool> generatedDocumentCache;
-            private INamedTypeSymbol cancellationTokenType;
-
-            public Analyzer(ConcurrentDictionary<SyntaxTree, bool> generatedDocumentCache, Compilation compilation)
-            {
-                this.generatedDocumentCache = generatedDocumentCache;
-                this.cancellationTokenType = compilation.GetTypeByMetadataName("System.Threading.Thread");
-            }
-
-            internal void HandleMethodDeclaration(SymbolAnalysisContext context)
-            {
-                var symbol = (IMethodSymbol)context.Symbol;
-                // TODO: decide whether to subscribe for methods or method calls
-                // http://stackoverflow.com/questions/29614112/how-to-get-invoked-method-name-in-roslyn
-                // begin my section
-                //var methodRoot = symbol.Construct()
-                //methodRoot.SyntaxTree
-                // end my section
-
-                if (this.cancellationTokenType == null)
-                {
-                    return;
-                }
-
-                if (symbol.DeclaredAccessibility == Accessibility.Private || symbol.DeclaredAccessibility == Accessibility.NotApplicable)
-                {
-                    return;
-                }
-
-                if (symbol.IsImplicitlyDeclared)
-                {
-                    return;
-                }
-
-                switch (symbol.MethodKind)
-                {
-                    case MethodKind.LambdaMethod:
-                    case MethodKind.Constructor:
-                    case MethodKind.Conversion:
-                    case MethodKind.UserDefinedOperator:
-                    case MethodKind.PropertyGet:
-                    case MethodKind.PropertySet:
-                    case MethodKind.StaticConstructor:
-                    case MethodKind.BuiltinOperator:
-                    case MethodKind.Destructor:
-                    case MethodKind.EventAdd:
-                    case MethodKind.EventRaise:
-                    case MethodKind.EventRemove:
-                        // These can't be async
-                        return;
-
-                    case MethodKind.DelegateInvoke:
-                        // Not sure if this is reachable
-                        return;
-
-                    case MethodKind.ExplicitInterfaceImplementation:
-                        // These are ignored
-                        return;
-
-                    case MethodKind.ReducedExtension:
-                    case MethodKind.Ordinary:
-                    case MethodKind.DeclareMethod:
-                    default:
-                        break;
-                }
-
-                if (!symbol.IsInAnalyzedSource(this.generatedDocumentCache, context.CancellationToken))
-                {
-                    return;
-                }
-
-                if (!symbol.HasAsyncSignature())
-                {
-                    return;
-                }
-
-                if (symbol.IsOverrideOrImplementation())
-                {
-                    return;
-                }
-
-                if (symbol.IsTestMethod())
-                {
-                    return;
-                }
-
-                foreach (var parameterSymbol in symbol.Parameters)
-                {
-                    if (parameterSymbol.RefKind == RefKind.Out)
-                    {
-                        continue;
-                    }
-
-                    INamedTypeSymbol parameterType = parameterSymbol.Type as INamedTypeSymbol;
-                    if (this.cancellationTokenType.Equals(parameterType))
-                    {
-                        return;
-                    }
-                }
-
-                foreach (var parameterSymbol in symbol.Parameters)
-                {
-                    if (parameterSymbol.RefKind == RefKind.Out)
-                    {
-                        continue;
-                    }
-
-                    foreach (var member in parameterSymbol.Type.GetMembers(nameof(CancellationToken)))
-                    {
-                        if (member.Kind != SymbolKind.Property)
-                        {
-                            continue;
-                        }
-
-                        if (member.DeclaredAccessibility != Accessibility.Public)
-                        {
-                            continue;
-                        }
-
-                        var propertySymbol = (IPropertySymbol)member;
-                        if (this.cancellationTokenType.Equals(propertySymbol.Type))
-                        {
-                            return;
-                        }
-                    }
-                }
-
-                context.ReportDiagnostic(Diagnostic.Create(Descriptor, symbol.Locations[0], symbol.Name));
-
-            }
+            return methodDeclaration.Modifiers.Any(x => x.Kind() == SyntaxKind.AsyncKeyword);
         }
     }
 }
