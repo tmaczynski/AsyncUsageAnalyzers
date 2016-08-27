a7aff0ff6a6434c41af972e710d5d31ab0ad38a8
Tomasz Maczyński <t.maczynski@gmail.com>
2016-08-27 10:19:59 +0200

    add a template of DontUseThreadSleepCommonTests.cs to test project

---
+++ AsyncUsageAnalyzers/AsyncUsageAnalyzers.Test/Usage/DontUseThreadSleepCommonTests.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+/* Contributor: Tomasz Maczyński */
+
+namespace AsyncUsageAnalyzers.Test.Usage
+{
+    using AsyncUsageAnalyzers.Usage;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using TestHelper;
+    using Xunit;
+
+    public abstract class DontUseThreadSleepCommonTests : DiagnosticVerifier
+    {
+    }
+}
