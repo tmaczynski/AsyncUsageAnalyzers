d28ea532d1055f2cf16d21561c95e7424db05d0a
Tomasz Maczyński <t.maczynski@gmail.com>
2016-08-29 23:45:40 +0200

    Create templates code fixers for DontUseThreadSleep-related analyzis

---
+++ AsyncUsageAnalyzers/AsyncUsageAnalyzers.CodeFixes/Usage/DontUseThreadSleepCodeCodeFixProvider.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+/* Contributor: Tomasz Maczyński */
+
+namespace AsyncUsageAnalyzers.Usage
+{
+    using System.Collections.Immutable;
+    using System.Composition;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Helpers;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CodeActions;
+    using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Formatting;
+
+    internal class DontUseThreadSleepCodeCodeFixProvider
+    {
+    }
+}
