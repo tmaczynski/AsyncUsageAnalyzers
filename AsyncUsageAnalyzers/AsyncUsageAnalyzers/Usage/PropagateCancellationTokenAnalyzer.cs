04a45a08ae2861580d333d1f599fc8c25313d579
Tomasz Maczyński <t.maczynski@gmail.com>
2017-05-13 05:08:19 +0200

    create placeholder files for PropagateCancellationToken analyzer and code fix

---
+++ AsyncUsageAnalyzers/AsyncUsageAnalyzers/Usage/PropagateCancellationTokenAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace AsyncUsageAnalyzers.Usage
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Helpers;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    /// <summary>
+    /// This analyzer which reports a diagnostic if <see cref="CancellationToken.None"/>.
+    /// is explicitly provided in a method call, but another cancellation token is available in the current context.
+    /// </summary>
+    internal class PropagateCancellationTokenAnalyzer
+    {
+    }
+}
---
+++ documentation/PropagateCancellationTokenAnalyzer.md
@@ -0,0 +1,40 @@
+﻿## PropagateCancellationToken
+
+<table>
+<tr>
+  <td>TypeName</td>
+  <td>PropagateCancellationToken</td>
+</tr>
+<tr>
+  <td>CheckId</td>
+  <td>PropagateCancellationToken</td>
+</tr>
+<tr>
+  <td>Category</td>
+  <td>Usage Rules</td>
+</tr>
+</table>
+
+## Cause
+
+`CancellationToken.None` is explicitly provided in a call, but another cancellation token is available in the current context.
+
+## Rule description
+
+TODO
+
+## How to fix violations
+
+Use one of tokens avaliable in the scope.
+
+## How to suppress violations
+
+```csharp
+[SuppressMessage("AsyncUsage.CSharp.Usage", "PropagateCancellationToken", Justification = "Reviewed.")]
+```
+
+```csharp
+#pragma warning disable PropagateCancellationToken
+Method(CancellationToken.None)
+#pragma warning restore PropagateCancellationToken
+```
