9ebee3b2cc8d45d5d641720c22d5fbb80ddfb1d5
Tomasz Maczyński <t.maczynski@gmail.com>
2017-05-14 00:16:36 +0200

    add first meaningful test - TestCancellationTokenNoneRisesDiagnosticIfTheresAnotherTokenAsync

--- AsyncUsageAnalyzers/AsyncUsageAnalyzers.Test/Usage/PropagateCancellationTokenUnitTests.cs
+++ AsyncUsageAnalyzers/AsyncUsageAnalyzers.Test/Usage/PropagateCancellationTokenUnitTests.cs
@@ -7,14 +7,50 @@ namespace AsyncUsageAnalyzers.Test.Usage
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using System.Threading;
     using System.Threading.Tasks;
-    using Microsoft.CodeAnalysis.Diagnostics;
     using AsyncUsageAnalyzers.Usage;
-    using TestHelper;
     using Microsoft.CodeAnalysis.CodeFixes;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using TestHelper;
+    using Xunit;
 
-    internal class PropagateCancellationTokenUnitTests : DiagnosticVerifier
+    public class PropagateCancellationTokenUnitTests : DiagnosticVerifier
     {
+        [Fact]
+        public async Task TestCancellationTokenNoneRisesDiagnosticIfTheresAnotherTokenAsync()
+        {
+            string testCode = @"
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    class CancellationTokenNoneTest
+    {
+        async Task<int> CalledAsyncMethodShortAsync(CancellationToken ct) =>
+            await Task.FromResult(0);
+
+        async Task CallingAsyncMethodShortAsync(CancellationToken ct) =>
+            await CalledAsyncMethodShortAsync(CancellationToken.None);
+
+        async Task<int> CalledAsyncMethodLongAsync(CancellationToken ct)
+        {
+            return await Task.FromResult(0);
+        }
+
+        async Task CallingAsyncMethodLongAsync(CancellationToken ct)
+        {
+            await CalledAsyncMethodLongAsync(CancellationToken.None);
+        }
+    }";
+
+            DiagnosticResult[] expected =
+            {
+                this.CSharpDiagnostic().WithLocation(11, 47),
+                this.CSharpDiagnostic().WithLocation(20, 46)
+            };
+            await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
+        }
+
         protected override IEnumerable<DiagnosticAnalyzer> GetCSharpDiagnosticAnalyzers()
         {
             yield return new PropagateCancellationTokenAnalyzer();
