86a0f957657470b8ec3b984a5ad03355cc1be535
Tomasz Maczyński <t.maczynski@gmail.com>
2017-05-15 01:39:09 +0200

    simplify tested code

--- AsyncUsageAnalyzers/AsyncUsageAnalyzers.Test/Usage/PropagateCancellationTokenUnitTests.cs
+++ AsyncUsageAnalyzers/AsyncUsageAnalyzers.Test/Usage/PropagateCancellationTokenUnitTests.cs
@@ -23,33 +23,33 @@ namespace AsyncUsageAnalyzers.Test.Usage
         public async Task TestCancellationTokenNoneRisesDiagnosticIfTheresAnotherTokenAsync(string cancellationTokenString)
         {
             var testCodeTemplate = @"
-    using System.Threading;
-    using System.Threading.Tasks;
+using System.Threading;
+using System.Threading.Tasks;
 
-    class CancellationTokenNoneTest
+class C
+{
+    async Task<int> CalledAsyncMethodShortAsync(CancellationToken ct) =>
+        await Task.FromResult(0);
+
+    async Task CallingAsyncMethodShortAsync(CancellationToken ct) =>
+        await CalledAsyncMethodShortAsync(##);
+
+    async Task<int> CalledAsyncMethodLongAsync(CancellationToken ct)
     {
-        async Task<int> CalledAsyncMethodShortAsync(CancellationToken ct) =>
-            await Task.FromResult(0);
+        return await Task.FromResult(0);
+    }
 
-        async Task CallingAsyncMethodShortAsync(CancellationToken ct) =>
-            await CalledAsyncMethodShortAsync(##);
-
-        async Task<int> CalledAsyncMethodLongAsync(CancellationToken ct)
-        {
-            return await Task.FromResult(0);
-        }
-
-        async Task CallingAsyncMethodLongAsync(CancellationToken ct)
-        {
-            await CalledAsyncMethodLongAsync(##);
-        }
-    }";
+    async Task CallingAsyncMethodLongAsync(CancellationToken ct)
+    {
+        await CalledAsyncMethodLongAsync(##);
+    }
+}";
             var testCode = testCodeTemplate.Replace("##", cancellationTokenString);
 
             DiagnosticResult[] expected =
             {
-                this.CSharpDiagnostic().WithArguments("CalledAsyncMethodShortAsync").WithLocation(11, 47),
-                this.CSharpDiagnostic().WithArguments("CalledAsyncMethodLongAsync").WithLocation(20, 46)
+                this.CSharpDiagnostic().WithArguments("CalledAsyncMethodShortAsync").WithLocation(11, 43),
+                this.CSharpDiagnostic().WithArguments("CalledAsyncMethodLongAsync").WithLocation(20, 42)
             };
             await this.VerifyCSharpDiagnosticAsync(testCode, expected, CancellationToken.None).ConfigureAwait(false);
         }
@@ -62,7 +62,7 @@ namespace AsyncUsageAnalyzers.Test.Usage
             var testCodeTemplate = @"
 using System.Threading;
 
-class TestCancellationTokenNoneIsUsedNotAsArgument
+class C
 {
     CancellationToken cancellationTokenField = ##;
 
